faa6cb00f865770c0040ff321291f04ad4088de9
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-05-10 20:23:31 +0300

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+(function(){
+function sum(a,b) {
+    let result = a + b;
+    return result;
+}
+
+function mult(a,b) {
+    let result = a * b;
+    return result;
+}
+
+function min(a,b) {
+    let result = a - b;
+    return result;
+}
+
+console.log(sum(3,4));
+console.log(mult(5,4));
+console.log(min(6,4));
+})();
